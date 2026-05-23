3e63e3358f522101f1ff5e30afede863b2049759
Lautaro Campregher <lautaronicolas201422@gmail.com>
2026-05-23 13:16:55 -0300

    css,partial,helpers

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import ProductManager from "./dao/ProductManager.js";
 
 const app = express();
 
-app.engine("handlebars", engine());
+app.engine(
+  "handlebars",
+  engine({
+    helpers: {
+      isStock: (stock, min) => stock > min,
+    },
+    partialsDir: root + "/views/partials",
+  }),
+);
 app.set("view engine", "handlebars");
 app.set("views", root + "/views");
 
@@ -13,19 +21,26 @@ app.listen(3000, () => {
   console.log("server ok");
 });
 
+app.use(express.static(root + "/public"));
+app.use(express.json(), express.urlencoded({ extended: true }));
+
 //CRUD
 //leer
 //primero un string y luego argumentos (handlers)
 app.get("/", async (req, res, next) => {
-  res.render("index");
+  res.render("index", {
+    styles: "/css/index.css",
+  });
 });
 
-app.get("/products",async(req,res,next)=>{
-  try{
-    const products= await ProductManager.getProducts()
-    res.render("products",{products,message:"Catalogo de Productos"});
-  }catch (error) {
+app.get("/products", async (req, res, next) => {
+  try {
+    const products = await ProductManager.getProducts();
+    res.render("products", {
+      products,
+      message: "Catalogo de Productos",
+    });
+  } catch (error) {
     console.log(error);
   }
-})
-
+});
