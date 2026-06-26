ee6b0037e6b53a7adaa75985e63b568c57c0859d
Lautaro Campregher <lautaronicolas201422@gmail.com>
2026-06-26 16:48:44 -0300

    websockets finish

--- src/public/js/realtime-products.js
+++ src/public/js/realtime-products.js
@@ -6,6 +6,33 @@ socket.on("connect", () => {
   console.log("Socket conectado");
 });
 
+const container = document.getElementById("products-container");
+
 socket.on("productsUpdated", (products) => {
   console.log("Evento recibido", products);
+
+  container.innerHTML = "";
+
+  products.forEach((product) => {
+    container.innerHTML += `
+      <ul>
+        <li>
+          <p>${product.name}</p>
+          <p>${product.price}</p>
+          <p>${product.description}</p>
+          <p>${product.stock}</p>
+
+          <img
+            src="/img/products/${product.thumbnails[0]}"
+            alt="${product.name}"
+            width="200"
+          >
+
+          <a href="/products/${product._id}">
+            Ver detalle
+          </a>
+        </li>
+      </ul>
+    `;
+  });
 });
